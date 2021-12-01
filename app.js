0f5d4307ae634609142d6b46c5dbf77a00123d11
MD Mehedi Hasan <tuhenmd28@gmail.com>
2021-12-01 12:20:38 +0600

    complite project

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+        let btn = document.querySelectorAll(".btn1");
+     let slideRow = document.querySelector(".slider-row");
+    
+     btn[0].onclick = function(){
+        slideRow.style.transform = "translateX(0%)";
+        for(i=0; i<3;i++){
+            btn[i].classList.remove('active')
+        }
+        this.classList.add('active')
+     }
+     btn[1].onclick = function(){
+        slideRow.style.transform = "translateX(-33.353333333334%)";
+        for(i=0; i<3;i++){
+            btn[i].classList.remove('active')
+        }
+        this.classList.add('active')
+     }
+     btn[2].onclick = function(){
+        slideRow.style.transform = "translateX(-66.676666666667%)";
+        for(i=0; i<3;i++){
+            btn[i].classList.remove('active')
+        }
+        this.classList.add('active')
+     }
+ 
